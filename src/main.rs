3ad8fb66870d16e5aa011b8a9b00770a75ff3abb
Martin Leuner <martin.leuner@rwth-aachen.de>
2021-04-18 13:50:47 +0200

    rust app listening to a port
    Have a single-threaded app listening to a port. Write output for
    successful connection attempts. Do nothing else.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: process arguments
+    // TODO: set up logging
+    // TODO: daemonize
+
+    let listener = TcpListener::bind("127.0.0.1:1337").expect("Failed to listen on port 1337");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
