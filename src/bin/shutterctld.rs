60bec58a9d36eb9eceba25ed7f0174c30e9b0f58
Martin Leuner <martin.leuner@rwth-aachen.de>
2021-11-01 23:13:17 +0100

    lib: create a FlatBuffer
    Write some short helpers to create a shuttermsg FlatBuffer.
    Not sending anything yet, that's the next step.
    
    Silence a warning in the (demo) code in shutterctld.
    
    Remove namespaces in FB schemas, as they are really covered by the crate
    names.

--- src/bin/shutterctld.rs
+++ src/bin/shutterctld.rs
@@ -8,7 +8,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:1337").expect("Failed to listen on port 1337");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let _stream = stream.unwrap();
         println!("Connection established!");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+#[allow(dead_code, unused_imports)]
 #[path = "../target/flatbuffers/shuttermsg_head_generated.rs"]
+mod shutterheader;
+#[allow(dead_code, unused_imports)]
 #[path = "../target/flatbuffers/shuttermsg_generated.rs"]
-pub mod shuttermsg;
+mod shuttermsg;
+
+mod shutterproto {
+    pub mod rpc {
+        use crate::shuttermsg::{CmdSystemState,
+                                CmdSystemStateArgs,
+                                Message,
+                                Shuttermsg,
+                                ShuttermsgArgs};
+
+        pub fn command_message<'a, T>(builder: &'a mut flatbuffers::FlatBufferBuilder,
+                                      msg_type: Message,
+                                      msg_data: flatbuffers::WIPOffset<T>) -> &'a [u8]
+        {
+            let msg = Shuttermsg::create(builder,
+                                         &ShuttermsgArgs{msg_type: msg_type,
+                                         msg: Some(msg_data.as_union_value())});
+            builder.finish(msg, None);
+            builder.finished_data()
+        }
+
+        pub fn get_state()
+        {
+            let mut fbb = flatbuffers::FlatBufferBuilder::new();
+            let data = CmdSystemState::create(&mut fbb, &CmdSystemStateArgs{});
+            let cmd_buf = command_message(&mut fbb, Message::CmdSystemState, data);
+            // TODO: send the command buffer through a TCP stream, wait for an answer
+        }
+    }
+}
