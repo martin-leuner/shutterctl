58a500904a7f31dbc394f77dc39668f7ae16f9cb
Martin Leuner <martin.leuner@rwth-aachen.de>
2022-04-18 20:17:09 +0200

    lib: add unparser for header messages
    Add struct shutterproto::transport::Session to handle secure tunnel
    state, wrapping and unwrapping data, formatting the header messages, and
    sending/receiving data over TcpStreams.
    
    Implement send(), stub some other methods.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,75 @@ mod shutterheader;
 mod shuttermsg;
 
 mod shutterproto {
+    mod transport {
+        use std::io::{self, BufReader, BufWriter, Write};
+        use std::net::TcpStream;
+
+        use crate::shutterheader::{Shutterheader,
+                                   ShutterheaderArgs,
+                                   CryptoParam,
+                                   NaClSecretBox,
+                                   NaClSecretBoxArgs,
+                                   Plain,
+                                   PlainArgs,
+                                   Version};
+
+        pub struct Session {
+            reader: BufReader<TcpStream>,
+            writer: BufWriter<TcpStream>,
+            id: u8,
+        }
+
+        impl Session {
+            pub fn new(stream: TcpStream) -> io::Result<Self> {
+                let reader = io::BufReader::new(stream.try_clone()?);
+                let writer = io::BufWriter::new(stream);
+                Ok(Self{reader, writer, id: 0})
+            }
+
+            pub fn _auth(&mut self, _user: &str, _key: &str) -> io::Result<()> {
+                // TODO
+                Ok(())
+            }
+
+            fn send(&mut self, payload: &[u8]) -> io::Result<()> {
+                let mut fbb = flatbuffers::FlatBufferBuilder::new();
+
+                let (param, crypt_type) = if self.id == 0 {
+                    let param = Plain::create(&mut fbb, &PlainArgs{});
+                    (param.as_union_value(), CryptoParam::Plain)
+                } else {
+                    // TODO: wrap, fill NaClSecretBox parameters...
+                    let param = NaClSecretBox::create(&mut fbb,
+                                                      &NaClSecretBoxArgs{
+                                                          session: self.id,
+                                                          nonce: None,
+                                                          mac: None
+                                                      });
+                    (param.as_union_value(), CryptoParam::NaClSecretBox)
+                };
+
+                let payload = fbb.create_vector_direct(payload);
+
+                let header = Shutterheader::create(&mut fbb,
+                                                   &ShutterheaderArgs{
+                                                       version: Version::Initial,
+                                                       crypt_type: crypt_type,
+                                                       crypt: Some(param),
+                                                       payload: Some(payload)
+                                                   });
+                fbb.finish_size_prefixed(header, Some("SHTRMSG"));
+
+                self.writer.write_all(fbb.finished_data())?;
+                Ok(())
+            }
+
+            fn _receive(&mut self) -> io::Result<&[u8]> {
+                Ok(&[])
+            }
+        }
+    }
+
     pub mod rpc {
         use crate::shuttermsg::{CmdSystemState,
                                 CmdSystemStateArgs,
@@ -13,10 +82,9 @@ mod shutterproto {
                                 Shuttermsg,
                                 ShuttermsgArgs};
 
-        pub fn command_message<'a, T>(builder: &'a mut flatbuffers::FlatBufferBuilder,
+        fn command_message<'a, T>(builder: &'a mut flatbuffers::FlatBufferBuilder,
                                       msg_type: Message,
-                                      msg_data: flatbuffers::WIPOffset<T>) -> &'a [u8]
-        {
+                                      msg_data: flatbuffers::WIPOffset<T>) -> &'a [u8] {
             let msg = Shuttermsg::create(builder,
                                          &ShuttermsgArgs{msg_type: msg_type,
                                          msg: Some(msg_data.as_union_value())});
@@ -24,8 +92,7 @@ mod shutterproto {
             builder.finished_data()
         }
 
-        pub fn get_state()
-        {
+        pub fn get_state() {
             let mut fbb = flatbuffers::FlatBufferBuilder::new();
             let data = CmdSystemState::create(&mut fbb, &CmdSystemStateArgs{});
             let cmd_buf = command_message(&mut fbb, Message::CmdSystemState, data);
