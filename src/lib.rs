0032deec770807cb0b150aab860041d9293501aa
Martin Leuner <martin.leuner@rwth-aachen.de>
2022-04-19 23:46:21 +0200

    lib: start receive() implementation
    Wrap other third party libs' errors in shutterproto::Error to be able to
    return a single Result type.
    FlatBuffers does not provide decent accessors (or even internal checks,
    apparently) for either file identifiers or prefixed sizes.
    Thus, write magic number and size prefix explicitly instead of using
    FlatBuffers's finish_size_prefixed() with a Some() file identifier.

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,33 @@ mod shutterheader;
 mod shuttermsg;
 
 mod shutterproto {
+    pub enum Error {
+        Io(std::io::Error),
+        FB(flatbuffers::InvalidFlatbuffer),
+    }
+
+    impl From<std::io::Error> for Error {
+        fn from(e: std::io::Error) -> Self {
+            Error::Io(e)
+        }
+    }
+
+    impl From<flatbuffers::InvalidFlatbuffer> for Error {
+        fn from(e: flatbuffers::InvalidFlatbuffer) -> Self {
+            Error::FB(e)
+        }
+    }
+
+    type Result<T> = std::result::Result<T, Error>;
+
+
     mod transport {
-        use std::io::{self, BufReader, BufWriter, Write};
+        use std::convert::TryInto;
+        use std::io::{self, BufRead, BufReader, BufWriter, Write};
         use std::net::TcpStream;
 
-        use crate::shutterheader::{Shutterheader,
+        use crate::shutterheader::{self,
+                                   Shutterheader,
                                    ShutterheaderArgs,
                                    CryptoParam,
                                    NaClSecretBox,
@@ -19,6 +41,7 @@ mod shutterproto {
                                    PlainArgs,
                                    Version};
 
+        const MAGIC: &[u8] = b"SHTR";
 
         pub struct Session<'a> {
             reader: BufReader<TcpStream>,
@@ -28,19 +51,19 @@ mod shutterproto {
         }
 
         impl<'a> Session<'a> {
-            pub fn new(stream: TcpStream) -> io::Result<Self> {
+            pub fn new(stream: TcpStream) -> crate::shutterproto::Result<Self> {
                 let reader = io::BufReader::new(stream.try_clone()?);
                 let writer = io::BufWriter::new(stream);
                 let fbb = flatbuffers::FlatBufferBuilder::new();
                 Ok(Self{reader, writer, fbb, id: 0})
             }
 
-            pub fn _auth(&mut self, _user: &str, _key: &str) -> io::Result<()> {
+            pub fn _auth(&mut self, _user: &str, _key: &str) -> crate::shutterproto::Result<()> {
                 // TODO
                 Ok(())
             }
 
-            pub fn send(&mut self, payload: &[u8]) -> io::Result<()> {
+            pub fn send(&mut self, payload: &[u8]) -> crate::shutterproto::Result<()> {
                 self.fbb.reset();
 
                 let (param, crypt_type) = if self.id == 0 {
@@ -66,19 +89,27 @@ mod shutterproto {
                                                        crypt: Some(param),
                                                        payload: Some(payload)
                                                    });
-                self.fbb.finish_size_prefixed(header, Some("SHTRMSG"));
+                self.fbb.finish(header, None);
+                let buf = self.fbb.finished_data();
+                let buf_len: u32 = buf.len().try_into().unwrap();
 
+                self.writer.write_all(MAGIC)?;
+                self.writer.write_all(&buf_len.to_le_bytes())?;
                 self.writer.write_all(self.fbb.finished_data())?;
+                self.writer.flush()?;
                 Ok(())
             }
 
-            pub fn receive(&mut self) -> io::Result<&[u8]> {
-                // TODO
+            pub fn receive(&mut self) -> crate::shutterproto::Result<&[u8]> {
+                let answ = self.reader.fill_buf()?;
+                // TODO: read magic & size
+                let header = shutterheader::root_as_shutterheader(answ)?;
+                // TODO: unwrap payload
                 Ok(&[])
             }
 
             #[inline]
-            pub fn exec_cmd(&mut self, payload: &[u8]) -> io::Result<&[u8]> {
+            pub fn exec_cmd(&mut self, payload: &[u8]) -> crate::shutterproto::Result<&[u8]> {
                 self.send(payload)?;
                 self.receive()
             }
@@ -86,7 +117,6 @@ mod shutterproto {
     }
 
     pub mod rpc {
-        use std::io;
         use std::net::TcpStream;
 
         use crate::shuttermsg::{CmdSystemState,
@@ -113,7 +143,7 @@ mod shutterproto {
         }
 
         impl<'a> Conn<'a> {
-            pub fn new(stream: TcpStream) -> io::Result<Self> {
+            pub fn new(stream: TcpStream) -> crate::shutterproto::Result<Self> {
                 let session = Session::new(stream)?;
                 let fbb = flatbuffers::FlatBufferBuilder::new();
                 Ok(Self{session, fbb})
