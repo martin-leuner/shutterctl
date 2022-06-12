f56bd1b08cae217b923b44e3a4957efbcf6ff792
Martin Leuner <martin.leuner@rwth-aachen.de>
2022-06-12 14:01:18 +0200

    shutterctld: parse config file of motors
    Basic TOML parsing for config files like this:
      [[motor]]
      name = "some description"
      id = 1
      runtime_ms = 25000
    
      [[motor]]
      name = "another description"
      id = 2
      runtime_ms = 33000

--- src/bin/shutterctld/shutterctl.rs
+++ src/bin/shutterctld/shutterctl.rs
@@ -1,4 +1,33 @@
+use serde_derive::Deserialize;
+
 pub struct Motor {
+    config: MotorConfig,
+    state: MotorState,
+}
+
+#[derive(Deserialize)]
+pub struct MotorConfig {
+    name: String,
+    id: usize,
+    runtime_ms: u32,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    motor: Vec<MotorConfig>,
+}
+
+pub struct MotorState {
+    state: CurrentMove,
+    known_min_percentage: u8,
+    known_max_percentage: u8,
+    last_stop: std::time::Instant,
+}
+
+pub enum CurrentMove {
+    Stopped,
+    Up,
+    Down,
 }
 
 pub struct System {
@@ -7,13 +36,30 @@ pub struct System {
 
 impl System {
     pub fn from_config() -> anyhow::Result<Self> {
-        let mut motors = Vec::<Option<Motor>>::new();
         if let Some(proj_dirs) = directories::ProjectDirs::from("", "", "shutterctl") {
-            let _cfgfile = proj_dirs.config_dir().join("config");
-            // TODO: parse cfgfile as TOML, fill motors accordingly
+            let cfgfile = proj_dirs.config_dir().join("config");
+            let cfgfile = std::fs::read_to_string(cfgfile)?;
+            let config: Config = toml::from_str(&cfgfile)?;
+
+            let max_id = config.motor.iter().max_by_key(|x| x.id).unwrap().id;
+            let mut motors = Vec::<Option<Motor>>::new();
+            motors.resize_with(max_id, || None);
+
+            for motor in config.motor {
+                let id = motor.id;
+                motors[id] = Some(Motor{
+                    config: motor,
+                    state: MotorState{
+                        state: CurrentMove::Stopped,
+                        known_min_percentage: 0,
+                        known_max_percentage: 100,
+                        last_stop: std::time::Instant::now(),
+                    }});
+            }
+            Ok(Self{motors})
         } else {
-            // TODO: return some error
+            // TODO: logging
+            Err(anyhow::anyhow!("No config file found"))
         }
-        Ok(Self{motors})
     }
 }
