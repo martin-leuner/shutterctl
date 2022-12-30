7be010ebbff3e45743accc7d09833c8a12edea17
Martin Leuner <martin.leuner@rwth-aachen.de>
2022-12-30 12:19:24 +0100

    shutterclient: Print get_state() output

--- src/bin/shutterclient/main.rs
+++ src/bin/shutterclient/main.rs
@@ -7,5 +7,15 @@ fn main() {
     let stream = TcpStream::connect("127.0.0.1:1337").expect("Failed to open connection to port 1337");
     let mut shutters = rpc::Conn::new(&stream).expect("Failed to create RPC connection object");
     // TODO: better error handling
-    shutters.get_state().expect("Error during `get_state` call");
+    let state = shutters.get_state().expect("Error during `get_state` call");
+
+    println!("Connected to shutter control daemon. State:");
+    for m in state.iter() {
+        println!(
+            "{name:>32}: {state} ({min}%-{max}%)",
+            name = m.config.name,
+            state = m.state.state,
+            min = m.state.known_min_percentage,
+            max = m.state.known_max_percentage);
+    }
 }
